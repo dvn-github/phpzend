a6e89578f783d063baf9b68a682c184bf263e37f
DVN <dvalle@interesses.com.mx>
2018-01-30 11:14:23 -0600

    Se limpia el Repositorio, dejando solo el Framework

--- application/default/controllers/IndexController.php
+++ application/default/controllers/IndexController.php
@@ -12,45 +12,11 @@ class IndexController extends Zend_Controller_Action
     	$this->_helper->layout->setLayout('main');
     }
 
-	public function registroAction(){
-    	$this->_helper->layout->setLayout('main');
-    	$this->view->headScript()->appendFile('/jquery/jquery.dataTables.min.js');
-    	$this->view->headScript()->appendFile('/js/registro.js');
-    	$this->view->headLink()->appendStylesheet($this->view->baseUrl . '/jquery/jquery.dataTables.min.css');  
-    }    
 
-    public function reloadAction() {
+    public function testAction() {
         $this->getHelper('layout')->disableLayout();
         $this->getHelper('ViewRenderer')->setNoRender();
-
-        $registros = new Registros();
-    	$data = $registros->getAll();
-        $paquete = array();
-        $paquete['draw'] = count($data);
-        $paquete['recordsTotal'] = count($data);
-        $paquete['recordsFiltered'] = count($data);
-        $data_parsed = array();
-        foreach ($data as $row) {
-            $item = array();
-            $item['registro'] = $row['registro'];
-            $item['fecha'] = $row['fecha'];
-            $data_parsed[] = $item;
-        }
-        $paquete['data'] = $data_parsed;
-
-        return $this->getResponse()->setBody(Zend_Json::encode($paquete));
-    }
-
-    public function addAction() {
-        $this->getHelper('layout')->disableLayout();
-        $this->getHelper('ViewRenderer')->setNoRender();
-        try{
-        	$registros = new Registros();
-	        $dato = $this->getRequest()->getParam("valor");
-	    	$registros->insert(array("id_usuario"=>"234", "registro" => $dato));
-        } catch(Exception $e){
-        	$error = $e->getMessage();
-        }
+        
 	        
        
 		$out = array('success' => true, 'error' => $error);
